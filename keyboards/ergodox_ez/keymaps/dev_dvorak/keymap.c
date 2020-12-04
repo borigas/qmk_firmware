b61b7fdb5f45df050dc408920797a80c0942a1c4
Ben Origas <borigas@ocuvera.com>
2020-12-03 19:11:09 -0600

    Tap dance layer switch testing

--- keyboards/ergodox_ez/keymaps/dev_dvorak/keymap.c
+++ keyboards/ergodox_ez/keymaps/dev_dvorak/keymap.c
@@ -20,6 +20,10 @@ enum custom_keycodes {
   RGB_SLD = EZ_SAFE_RANGE,
 };
 
+enum {
+    TD_WinMove
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
     LT(1,KC_GRAVE), KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_6,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       KC_SLASH,
@@ -29,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TT(5),          KC_LCTRL,       KC_LGUI,        KC_LALT,        LT(2,KC_LBRACKET),                                                                                                KC_RBRACKET,    KC_RALT,        KC_RGUI,        RCTL_T(KC_APPLICATION),TT(2),
                                                                                                     LALT(KC_F4),    KC_DELETE,      KC_TRANSPARENT, KC_ESCAPE,
                                                                                                                     KC_TRANSPARENT, KC_HYPR,
-                                                                                    LT(3,KC_ENTER), KC_BSPACE,      TT(2),          TT(3),          TT(1),          KC_SPACE
+                                                                                    LT(3,KC_ENTER), KC_BSPACE,      TD(TD_WinMove),          TT(3),          TT(1),          KC_SPACE
   ),
   [1] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F6,                                          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,         KC_TRANSPARENT,
@@ -81,6 +85,134 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
+};
+
+typedef enum {
+    SINGLE_TAP = 1,
+    SINGLE_HOLD,
+    DOUBLE_TAP,
+    DOUBLE_HOLD,
+    DOUBLE_SINGLE_TAP, // Send two single taps
+    TRIPLE_TAP,
+    TRIPLE_HOLD
+} td_state_t;
+
+static td_state_t td_state;
+
+
+/* Return an integer that corresponds to what kind of tap dance should be executed.
+ *
+ * How to figure out tap dance state: interrupted and pressed.
+ *
+ * Interrupted: If the state of a dance dance is "interrupted", that means that another key has been hit
+ *  under the tapping term. This is typically indicitive that you are trying to "tap" the key.
+ *
+ * Pressed: Whether or not the key is still being pressed. If this value is true, that means the tapping term
+ *  has ended, but the key is still being pressed down. This generally means the key is being "held".
+ *
+ * One thing that is currenlty not possible with qmk software in regards to tap dance is to mimic the "permissive hold"
+ *  feature. In general, advanced tap dances do not work well if they are used with commonly typed letters.
+ *  For example "A". Tap dances are best used on non-letter keys that are not hit while typing letters.
+ *
+ * Good places to put an advanced tap dance:
+ *  z,q,x,j,k,v,b, any function key, home/end, comma, semi-colon
+ *
+ * Criteria for "good placement" of a tap dance key:
+ *  Not a key that is hit frequently in a sentence
+ *  Not a key that is used frequently to double tap, for example 'tab' is often double tapped in a terminal, or
+ *    in a web form. So 'tab' would be a poor choice for a tap dance.
+ *  Letters used in common words as a double. For example 'p' in 'pepper'. If a tap dance function existed on the
+ *    letter 'p', the word 'pepper' would be quite frustating to type.
+ *
+ * For the third point, there does exist the 'DOUBLE_SINGLE_TAP', however this is not fully tested
+ *
+ */
+uint8_t cur_dance(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return SINGLE_TAP;
+        // Key has not been interrupted, but the key is still held. Means you want to send a 'HOLD'.
+        else return SINGLE_HOLD;
+    } else if (state->count == 2) {
+        // DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
+        // action when hitting 'pp'. Suggested use case for this return value is when you want to send two
+        // keystrokes of the key, and not the 'double tap' action/macro.
+        if (state->interrupted) return DOUBLE_SINGLE_TAP;
+        else if (state->pressed) return DOUBLE_HOLD;
+        else return DOUBLE_TAP;
+    }
+
+    // Assumes no one is trying to type the same letter three times (at least not quickly).
+    // If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
+    // an exception here to return a 'TRIPLE_SINGLE_TAP', and define that enum just like 'DOUBLE_SINGLE_TAP'
+    if (state->count == 3) {
+        if (state->interrupted || !state->pressed) return TRIPLE_TAP;
+        else return TRIPLE_HOLD;
+    } else return 8; // Magic number. At some point this method will expand to work for more presses
+}
+
+// Handle the possible states for each tapdance keycode you define:
+
+void winmove_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    uint8_t layer = 0;
+    bool isHold = false;
+    switch (td_state) {
+        case SINGLE_TAP:
+            layer = 1;
+            isHold = false;
+            break;
+        case SINGLE_HOLD:
+            layer = 1;
+            isHold = true;
+            break;
+        case DOUBLE_TAP:
+            layer = 2;
+            isHold = false;
+            break;
+        case DOUBLE_HOLD:
+            layer = 2;
+            isHold = true;
+            break;
+        case TRIPLE_TAP:
+            layer = 3;
+            isHold = false;
+            break;
+        case TRIPLE_HOLD:
+            layer = 3;
+            isHold = true;
+            break;
+        default:
+            break;
+    }
+    if(layer != 0) {
+        if(isHold) {
+            layer_on(layer);
+        } else {
+            bool isLayerOn = IS_LAYER_ON(layer);
+            layer_clear();
+            if(isLayerOn) {
+                layer_off(layer);
+            } else {
+                layer_on(layer);
+            }
+        }
+    }
+}
+
+void winmove_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case SINGLE_HOLD:
+        case DOUBLE_HOLD:
+        case TRIPLE_HOLD:
+            layer_clear();
+            break;
+        default:
+            break;
+    }
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_WinMove] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, winmove_finished, winmove_reset)
 };
 
 
